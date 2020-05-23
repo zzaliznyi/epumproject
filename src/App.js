ec7f448a6b7a1bb6aa4860aeb51497f91b255a5b
Zakhar <zahasenok@gmail.com>
2020-05-23 16:56:02 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ export class Header extends React.Component{
   }
   render(){
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-light  fixed-top ">
-  <div class="container">
+      <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark  fixed-top ">
+  
   <a class="navbar-brand" href="#">
     <img src={require(`${ImageLoader("logo")}`)} alt="" loading="lazy"></img>
   </a>
@@ -52,8 +53,8 @@ export class Header extends React.Component{
         </li>
       </ul>
     </div>
-  </div>
 </nav>
+</div>
     )
   }
 }
@@ -112,25 +113,25 @@ export class Biography extends React.Component{
           </div>
           <hr></hr>
           <div className="b_text">
-          Three Days Grace is a Canadian rock band formed in Norwood, Ontario in 1997. Based in Toronto, the band's original line-up consisted of guitarist and lead vocalist Adam Gontier, drummer and backing vocalist Neil Sanderson, and bassist Brad Walst. In 2003, Barry Stock was recruited as the band's lead guitarist, making them a four-member band. In 2013, Gontier left the band and was replaced by My Darkest Days' vocalist Matt Walst, who is also the younger brother of bassist Brad Walst.
+          Three Days Grace is a Canadian rock band formed in Norwood, Ontario in 1997. Based in Toronto, the band's original line-up consisted of guitarist and lead vocalist Adam Gontier, drummer and backing vocalist Neil Sanderson, and bassist Brad Walst. In 2003, Barry Stock was recruited as the band's lead guitarist, making them a four-member band.<br></br> In 2013, Gontier left the band and was replaced by My Darkest Days' vocalist Matt Walst, who is also the younger brother of bassist Brad Walst.<br></br>
 
-            Currently signed to RCA Records, they have released six studio albums, each at three-year intervals: Three Days Grace in 2003, One-X in 2006, Life Starts Now in 2009, Transit of Venus in 2012, Human in 2015, and Outsider in 2018. The first three albums have been RIAA certified 2x platinum, 3x platinum, and platinum, respectively, in the United States. In Canada, they have been certified by Music Canada as platinum, double platinum, and platinum, respectively. The band has had 15 No. 1 songs on the Billboard Hot Mainstream Rock Tracks chart, and three No. 1 hits on Alternative Songs.
-
-
-            Contents
-            1	History
-            1.1	Early years
-            1.2	One-X and Stock's arrival (2006–2008)
-            1.3	Life Starts Now (2009–2011)
-            1.4	Transit of Venus and Gontier's departure (2012–2013)
-            1.5	Human (2014–2017)
-            1.6	Outsider (2017–present)
-            2	Awards and records
-            3	Musical style
-            4	Members
-            5	Discography
-            6	References
-            7	External links
+            Currently signed to RCA Records, they have released six studio albums, each at three-year intervals: Three Days Grace in 2003, One-X in 2006, Life Starts Now in 2009, Transit of Venus in 2012, Human in 2015, and Outsider in 2018. The first three albums have been RIAA certified 2x platinum, 3x platinum, and platinum, respectively, in the United States. In Canada, they have been certified by Music Canada as platinum, double platinum, and platinum, respectively.<br></br> The band has had 15 No. 1 songs on the Billboard Hot Mainstream Rock Tracks chart, and three No. 1 hits on Alternative Songs.
+            <br></br>
+            <br></br>
+            Contents <br></br>
+            1	History<br></br>
+            1.1	Early years<br></br>
+            1.2	One-X and Stock's arrival (2006–2008)<br></br>
+            1.3	Life Starts Now (2009–2011)<br></br>
+            1.4	Transit of Venus and Gontier's departure (2012–2013)<br></br>
+            1.5	Human (2014–2017)<br></br>
+            1.6	Outsider (2017–present)<br></br>
+            2	Awards and records<br></br>
+            3	Musical style<br></br>
+            4	Members<br></br>
+            5	Discography<br></br>
+            6	References<br></br>
+            7	External links<br></br>
 
           </div>
       </main>
@@ -145,24 +146,119 @@ function photoTemplate(src)
     </div>
   )
 }
-function pagination(){
+class Pagination extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {page : 1,pages: 3};
+    this.b_state = this.b_state.bind(this);
+    this.b_num = this.b_num.bind(this);
+    this.page_up = this.page_up.bind(this);
+    this.page_down = this.page_down.bind(this);
+    this.next_state = this.next_state.bind(this);
+    this.prev_state = this.prev_state.bind(this);
+    this.b_able = this.b_able.bind(this);
+  }
+  b_state(el){
+    if(this.state.pages <=2)
+    {
+      if(this.state.page == el) return "active";
+      else return "";
+    }
+    else
+    {
+      if(this.state.page == 1 && el == 1) return "active";
+      else{
+        if(el == 3 && this.state.page == this.state.pages) return "active";
+        else{
+          if(el ==2 && (this.state.page != 1 && this.state.page != this.state.pages)) return "active";
+          else return "";
+        }
+      } 
+    }
+  }
+  b_num(el){
+    if(this.state.pages == 2)
+    {
+      return el;
+    }
+    else{
+      if(this.state.page != 1 && this.state.page != this.state.pages)
+      {
+        if(el == 1) return this.state.page-1;
+          if(el == 2) return this.state.page;
+          if(el == 3) return this.state.page + 1;
+      }
+      else
+      {
+        if(this.state.page == 1){
+          if(el == 1) return this.state.page;
+          if(el == 2) return this.state.page + 1;
+          if(el == 3) return this.state.page + 2;
+
+        }
+        if(this.state.page == this.state.pages){
+          if(el == 1) return this.state.page-2;
+          if(el == 2) return this.state.page-1;
+          if(el == 3) return this.state.page;
+        }
+      }
+  }
+    
+  }
+  b_able(el){
+      if(this.state.pages > 2) return "";
+      else{
+        if(this.state.pages == 1){
+          if(el == 1) return "";
+          else return " disabled";
+        }
+        else{
+          if(el == 3) return " disabled";
+          else return "";
+        }
+      }
+  }
+  page_up()
+  {
+    if(this.state.page != this.state.pages){
+      let nextPage = this.state.page + 1;
+      this.setState(state =>({
+        page: nextPage
+      }));
+    }
+  }
+  page_down(){
+    if(this.state.page != 1){
+      let prevPage = this.state.page - 1;
+      this.setState(state =>({
+        page: prevPage
+      }));
+    }
+  }
+  next_state(){
+    if(this.state.page == this.state.pages) return "disabled";
+    return "";
+  }
+  prev_state(){
+    if(this.state.page == 1) return "disabled";
+    return "";
+  }
+  render(){
   return(
-    <nav aria-label="...">
-  <ul class="pagination">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1">Previous</a>
+    <nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class={"page-item "+ this.prev_state()}>
+      <a class="page-link" href="#" onClick={this.page_down}>Previous</a>
     </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item active">
-      <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
+    <li className={"page-item "+ this.b_state(1) + this.b_able(1)} ><a class="page-link" href="#">{this.b_num(1)}</a></li>
+    <li class={"page-item "+ this.b_state(2) + this.b_able(2)}><a class="page-link" href="#">{this.b_num(2)}</a></li>
+    <li class={"page-item "+ this.b_state(3) + this.b_able(3)}><a class="page-link" href="#">{this.b_num(3)}</a></li>
+    <li class={"page-item " + this.next_state()}>
+      <a class="page-link" href="#" onClick={this.page_up}>Next</a>
     </li>
   </ul>
 </nav>
-  )
+  )}
 }
 export class Gallery extends React.Component{
   constructor(props)
@@ -189,7 +285,7 @@ export class Gallery extends React.Component{
   {
     const photos = this.loadPhotos(1);
     return(
-      <div className="gallery" onLoad={this.loadPhotos(this.state.c_page)}>
+      <div className="gallery" >
         <div className="g_heading">
           <h1>Gallery</h1>
         </div>
@@ -198,7 +294,7 @@ export class Gallery extends React.Component{
           {photos}
         </div>
         <div className="g_pagination">
-          {pagination()}
+          <Pagination/>
         </div>
       </div>
     )
